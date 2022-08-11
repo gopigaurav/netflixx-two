15071492729a7b2099e4d713d6b4b0f74c83305e
gopi <gopigaurav9@gmail.com>
2022-08-11 16:50:30 +0530

    main page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,46 +1,10 @@
 import type { NextPage } from "next";
 import Head from "next/head";
 import Image from "next/image";
-import { getProducts, Product } from '@stripe/firestore-stripe-payments'
-import { useRecoilValue } from 'recoil'
-import Banner from '../components/Banner'
-import Header from '../components/Header'
-import Modal from '../components/Modal'
-import Row from '../components/Row'
 
 const Home: NextPage = () => {
   return (
-    <div
-      className={`relative h-screen bg-gradient-to-b from-gray-900/10 to-[#010511] lg:h-[140vh] ${
-        showModal && "!h-screen overflow-hidden"
-      }`}
-    >
-      <Head>
-        <title>
-          {movie?.title || movie?.original_name || "Home"} - Netflix
-        </title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
-      <Header />
-
-      <main className="relative pl-4 pb-24 lg:space-y-24 lg:pl-16 ">
-        <Banner netflixOriginals={netflixOriginals} />
-
-        <section className="md:space-y-24">
-          <Row title="Trending Now" movies={trendingNow} />
-          <Row title="Top Rated" movies={topRated} />
-          <Row title="Action Thrillers" movies={actionMovies} />
-          {/* My List */}
-          {list.length > 0 && <Row title="My List" movies={list} />}
-
-          <Row title="Comedies" movies={comedyMovies} />
-          <Row title="Scary Movies" movies={horrorMovies} />
-          <Row title="Romance Movies" movies={romanceMovies} />
-          <Row title="Documentaries" movies={documentaries} />
-        </section>
-      </main>
-      {showModal && <Modal />}
+    <div>
     </div>
   );
 };
